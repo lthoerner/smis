529a690c44ca1056def4335e1a77bbdaf9a03d76
Eyesonjune18 <lowellashton@gmail.com>
2022-12-13 19:24:51 -0800

    Added basic argument interpreter.

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,44 @@
 #![allow(unused_variables)]
 
 
+use std::path::Path;
+
+
 mod assembler;
 mod disassembler;
 mod emulator;
 mod utilities;
 
 
+const USAGE_ERROR: &str = "Usage: <target> <input_file> <output_file>
+Target must be either --assemble [-a] or --disassemble [-d].";
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 4 {
+        panic!("Incorrect number of arguments!\n{}", USAGE_ERROR);
+    }
+
+    let target = &args[1];
+    let input_file = &args[2];
+    let output_file = &args[3];
+
+    if !Path::new(input_file).exists() {
+        panic!("Input file does not exist!\n{}", USAGE_ERROR);
+    }
+
+    if !Path::new(output_file).exists() {
+        panic!("Output file does not exits!\n{}", USAGE_ERROR);
+    }
+
+    match target.as_str() {
+        "--assemble" | "-a" => match assembler::assemble(input_file, output_file) {
+            Ok(()) => println!("File assembled successfully."),
+            Err(_) => println!("File failed to assemble.")
+        },
+        // "--disassemble" | "-d" => disassembler::disassemble(input_file, output_file),
+        _ => panic!("Invalid target \"{}\"!\n{}", target, USAGE_ERROR)
+    }
 }
