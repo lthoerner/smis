9aee5b251cc4d065d665fab17e63940a2f3a5e41
Eyesonjune18 <lowellashton@gmail.com>
2022-12-13 17:52:02 -0800

    Added read_labels(). Needs better error handling.

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -1,10 +1,9 @@
-// TODO: Turn these off when no longer needed
-#![allow(unused_imports)]
-
 mod errors;
 
-use std::num::IntErrorKind;
-
+use std::fs::File;
+use std::io::{ BufRead, BufReader };
+use crate::utilities::symbol_table;
+use crate::utilities::symbol_table::SymbolTable;
 use crate::utilities::string_methods::SMISString;
 use self::errors::assembler_error::AssemblerError;
 use self::errors::assembler_error::FileHandlerError;
@@ -15,6 +14,42 @@ use self::errors::assembler_error::RegisterParseError;
 
 pub fn assemble(asm_file_name: &str, bin_file_name: &str) {
     
+}
+
+// Scans the input ASM file for labels, and adds them to the symbol table for use later
+fn read_labels(asm_file: &File) -> Result<SymbolTable, FileHandlerError> {
+    // Store the address of the instruction currently being scanned
+    let mut current_address: u16 = 0x00;
+    
+    // Stores all labels found in the file
+    let mut symbol_table = symbol_table::new();
+    
+    let scanner = BufReader::new(asm_file);
+
+    for line in scanner.lines() {
+        // Handle any errors for line reading
+        let line = match line {
+            Ok(text) => text,
+            Err(_) => return Err(FileHandlerError::ErrorInvalidFileContents)
+        };
+
+        let line = line.as_str();
+
+        // Add any labels to the symbol table
+        if is_label(line) {
+            symbol_table.add_label(match line.strip_suffix(':') {
+                Some(name) => name,
+                // TODO: Error type?
+                None => panic!()
+            }, current_address);
+        }
+
+        // Current address is incremented by 2 because all instructions
+        // are 32 bits, but the memory values are only 16 bits
+        if !is_blankline_comment_label(line) { current_address += 2; }
+    }
+
+    Ok(symbol_table)
 }
 
 fn get_register(instruction: &str, index: usize) -> Result<u8, RegisterParseError> {
@@ -43,7 +78,7 @@ fn parse_register(register: &str) -> Result<u8, RegisterParseError> {
     let register_num = match trimmed_register.parse::<u8>() {
         Ok(val) => val,
         Err(err) => match err.kind() {
-            IntErrorKind::PosOverflow => return Err(RegisterParseError::ErrorNumberOutOfBounds),
+            std::num::IntErrorKind::PosOverflow => return Err(RegisterParseError::ErrorNumberOutOfBounds),
             _ => return Err(RegisterParseError::ErrorNonNumeric)
         }
     };
@@ -72,7 +107,7 @@ fn parse_immediate(immediate: &str) -> Result<u16, ImmediateParseError> {
     match trimmed_immediate.parse::<u16>() {
         Ok(val) => Ok(val),
         Err(err) => match err.kind() {
-            IntErrorKind::PosOverflow => Err(ImmediateParseError::ErrorNumberOutOfBounds),
+            std::num::IntErrorKind::PosOverflow => Err(ImmediateParseError::ErrorNumberOutOfBounds),
             _ => Err(ImmediateParseError::ErrorNonNumeric)
         }
     }
