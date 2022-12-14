f689fd9478c035919fa5e410e6a08009a7023b30
Eyesonjune18 <lowellashton@gmail.com>
2022-12-13 19:23:38 -0800

    Added some garbage code for testing.

---
+++ src/disassembler/mod.rs
@@ -0,0 +1,7 @@
+pub fn disassemble(bin_file_name: &str, asm_file_name: &str) -> Result<(), ()> {
+    // Open the input and output file
+    // let bin_file = File::options().read(true).open(bin_file_name)?;
+    // let asm_file = File::options().write(true).create(true).open(asm_file_name)?;
+
+    Ok(())
+}
