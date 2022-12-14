8ce13f6f1da9072ee57ecdf176923c789579892a
Eyesonjune18 <lowellashton@gmail.com>
2022-12-13 16:09:30 -0800

    Modified a TODO.

--- src/utilities/string_methods.rs
+++ src/utilities/string_methods.rs
@@ -9,7 +9,7 @@ impl<'a> SMISString for &'a str {
     }
 
     fn get_word(&self, index: usize) -> &str {
-        // TODO: Add some actual error handling here
+        // TODO: Add some actual error handling here, or use Option
         if index > self.count_words() { panic!("Word {} out of bounds for string {} (size: {})", index, self, self.count_words()); }
         
         // Split the string into words and pick the word at the given index
