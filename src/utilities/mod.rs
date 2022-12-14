234ed9a02653f14d315856d90bf06002d56e69fe
Eyesonjune18 <lowellashton@gmail.com>
2022-12-13 21:55:44 -0800

    Added opcode_resolver.rs, allowing opcodes to be translated to mnemonics and vice versa.

--- src/utilities/mod.rs
+++ src/utilities/mod.rs
@@ -1,3 +1,4 @@
 pub mod instruction;
+pub mod opcode_resolver;
 pub mod string_methods;
 pub mod symbol_table;
---
+++ src/utilities/opcode_resolver.rs
@@ -0,0 +1,197 @@
+use super::instruction::Instruction;
+
+
+// Opcode constants
+const OP_SET: u8                = 0x01;
+const OP_COPY: u8               = 0x02;
+
+const OP_ADD: u8                = 0x03;
+const OP_SUBTRACT: u8           = 0x04;
+const OP_MULTIPLY: u8           = 0x05;
+const OP_DIVIDE: u8             = 0x06;
+const OP_MODULO: u8             = 0x07;
+
+const OP_COMPARE: u8            = 0x08;
+
+const OP_SHIFT_LEFT: u8         = 0x09;
+const OP_SHIFT_RIGHT: u8        = 0x0A;
+
+const OP_AND: u8                = 0x0B;
+const OP_OR: u8                 = 0x0C;
+const OP_XOR: u8                = 0x0D;
+const OP_NAND: u8               = 0x0E;
+const OP_NOR: u8                = 0x0F;
+const OP_NOT: u8                = 0x10;
+
+const OP_ADD_IMM: u8            = 0x11;
+const OP_SUBTRACT_IMM: u8       = 0x12;
+const OP_MULTIPLY_IMM: u8       = 0x13;
+const OP_DIVIDE_IMM: u8         = 0x14;
+const OP_MODULO_IMM: u8         = 0x15;
+
+const OP_COMPARE_IMM: u8        = 0x16;
+
+const OP_SHIFT_LEFT_IMM: u8     = 0x17;
+const OP_SHIFT_RIGHT_IMM: u8    = 0x18;
+
+const OP_AND_IMM: u8            = 0x19;
+const OP_OR_IMM: u8             = 0x1A;
+const OP_XOR_IMM: u8            = 0x1B;
+const OP_NAND_IMM: u8           = 0x1C;
+const OP_NOR_IMM: u8            = 0x1D;
+
+const OP_LOAD: u8               = 0x1E;
+const OP_STORE: u8              = 0x1F;
+
+const OP_JUMP: u8               = 0x20;
+const OP_JUMP_IF_ZERO: u8       = 0x21;
+const OP_JUMP_IF_NOTZERO: u8    = 0x22;
+const OP_JUMP_LINK: u8          = 0x23;
+
+const OP_HALT: u8               = 0x24;
+
+
+// Gets the associated mnemonic for a given opcode
+pub fn get_mnemonic(opcode: u8) -> Option<&'static str> {
+    Some(match opcode { 
+        OP_SET              => "SET",
+        OP_COPY             => "COPY",
+        OP_ADD              => "ADD",
+        OP_SUBTRACT         => "SUBTRACT",
+        OP_MULTIPLY         => "MULTIPLY",
+        OP_DIVIDE           => "DIVIDE",
+        OP_MODULO           => "MODULO",
+        OP_COMPARE          => "COMPARE",
+        OP_SHIFT_LEFT       => "SHIFT-LEFT",
+        OP_SHIFT_RIGHT      => "SHIFT-RIGHT",
+        OP_AND              => "AND",
+        OP_OR               => "OR",
+        OP_XOR              => "XOR",
+        OP_NAND             => "NAND",
+        OP_NOR              => "NOR",
+        OP_NOT              => "NOT",
+        OP_ADD_IMM          => "ADD-IMM",
+        OP_SUBTRACT_IMM     => "SUBTRACT-IMM",
+        OP_MULTIPLY_IMM     => "MULTIPLY-IMM",
+        OP_DIVIDE_IMM       => "DIVIDE-IMM",
+        OP_MODULO_IMM       => "MODULO-IMM",
+        OP_COMPARE_IMM      => "COMPARE-IMM",
+        OP_SHIFT_LEFT_IMM   => "SHIFT-LEFT-IMM",
+        OP_SHIFT_RIGHT_IMM  => "SHIFT-RIGHT-IMM",
+        OP_AND_IMM          => "AND-IMM",
+        OP_OR_IMM           => "OR-IMM",
+        OP_XOR_IMM          => "XOR-IMM",
+        OP_NAND_IMM         => "NAND-IMM",
+        OP_NOR_IMM          => "NOR-IMM",
+        OP_LOAD             => "LOAD",
+        OP_STORE            => "STORE",
+        OP_JUMP             => "JUMP",
+        OP_JUMP_IF_ZERO     => "JUMP-IF-ZERO",
+        OP_JUMP_IF_NOTZERO  => "JUMP-IF-NOTZERO",
+        OP_JUMP_LINK        => "JUMP-LINK",
+        OP_HALT             => "HALT",
+        _ => return None
+    })
+}
+
+// Gets the associated opcode for a given mnemonic
+pub fn get_opcode(mnemonic: &str) -> Option<u8> {
+    Some(match mnemonic { 
+        "set"               => OP_SET,
+        "copy"              => OP_COPY,
+        "add"               => OP_ADD,
+        "subtract"          => OP_SUBTRACT,
+        "multiply"          => OP_MULTIPLY,
+        "divide"            => OP_DIVIDE,
+        "modulo"            => OP_MODULO,
+        "compare"           => OP_COMPARE,
+        "shift-left"        => OP_SHIFT_LEFT,
+        "shift-right"       => OP_SHIFT_RIGHT,
+        "and"               => OP_AND,
+        "or"                => OP_OR,
+        "xor"               => OP_XOR,
+        "nand"              => OP_NAND,
+        "nor"               => OP_NOR,
+        "not"               => OP_NOT,
+        "add-imm"           => OP_ADD_IMM,
+        "subtract-imm"      => OP_SUBTRACT_IMM,
+        "multiply-imm"      => OP_MULTIPLY_IMM,
+        "divide-imm"        => OP_DIVIDE_IMM,
+        "modulo-imm"        => OP_MODULO_IMM,
+        "compare-imm"       => OP_COMPARE_IMM,
+        "shift-left-imm"    => OP_SHIFT_LEFT_IMM,
+        "shift-right-imm"   => OP_SHIFT_RIGHT_IMM,
+        "and-imm"           => OP_AND_IMM,
+        "or-imm"            => OP_OR_IMM,
+        "xor-imm"           => OP_XOR_IMM,
+        "nand-imm"          => OP_NAND_IMM,
+        "nor-imm"           => OP_NOR_IMM,
+        "load"              => OP_LOAD,
+        "store"             => OP_STORE,
+        "jump"              => OP_JUMP,
+        "jump-if-zero"      => OP_JUMP_IF_ZERO,
+        "jump-if-notzero"   => OP_JUMP_IF_NOTZERO,
+        "jump-link"         => OP_JUMP_LINK,
+        "halt"              => OP_HALT,
+        _ => return None
+    })
+}
+
+// Uses the given opcode to return an enum with fields based on the instruction format
+pub fn get_instruction_format(opcode: u8) -> Option<Instruction> {
+    Some(match opcode { 
+        OP_COPY
+        | OP_ADD
+        | OP_SUBTRACT
+        | OP_MULTIPLY
+        | OP_DIVIDE
+        | OP_MODULO
+        | OP_COMPARE
+        | OP_SHIFT_LEFT
+        | OP_SHIFT_RIGHT
+        | OP_AND
+        | OP_OR
+        | OP_XOR
+        | OP_NAND
+        | OP_NOR
+        | OP_NOT
+            => Instruction::RFormat {
+                opcode: 0,
+                r_dest: 0,
+                r_op1: 0,
+                r_op2: 0
+            },
+        OP_SET
+        | OP_ADD_IMM
+        | OP_SUBTRACT_IMM
+        | OP_MULTIPLY_IMM
+        | OP_DIVIDE_IMM
+        | OP_MODULO_IMM
+        | OP_COMPARE_IMM
+        | OP_SHIFT_LEFT_IMM
+        | OP_SHIFT_RIGHT_IMM
+        | OP_AND_IMM
+        | OP_OR_IMM
+        | OP_XOR_IMM
+        | OP_NAND_IMM
+        | OP_NOR_IMM
+        | OP_LOAD
+        | OP_STORE
+            => Instruction::IFormat {
+                opcode: 0,
+                r_dest: 0,
+                r_op1: 0,
+                i_op2: 0
+            },
+        OP_JUMP
+        | OP_JUMP_IF_ZERO
+        | OP_JUMP_IF_NOTZERO
+        | OP_JUMP_LINK
+        | OP_HALT
+            => Instruction::JFormat {
+                opcode: 0,
+                dest_addr: 0
+            },
+        _ => return None
+    })
+}
