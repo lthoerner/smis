6fd59f9829b32a1fce9df2b11363318d953f7491
Eyesonjune18 <lowellashton@gmail.com>
2022-12-14 20:02:22 -0800

    Added without_first_word() string method.

--- src/utilities/string_methods.rs
+++ src/utilities/string_methods.rs
@@ -1,6 +1,7 @@
 pub trait SMISString {
     fn count_words(&self) -> usize;
     fn get_word(&self, index: usize) -> Option<&str>;
+    fn without_first_word(&self) -> String;
 }
 
 impl<'a> SMISString for &'a str {
@@ -11,5 +12,14 @@ impl<'a> SMISString for &'a str {
     fn get_word(&self, index: usize) -> Option<&str> {
         // Split the string into words and pick the word at the given index
         self.split_whitespace().nth(index)
+    }
+    
+    // TODO: Option?
+    fn without_first_word(&self) -> String {
+        // Split the string into words and collect the words into a vector
+        let words: Vec<&str> = self.split_whitespace().collect();
+
+        // Join the words into a string, starting from the second word
+        words[1..].join(" ")
     }
 }
