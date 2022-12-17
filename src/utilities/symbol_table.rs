78e40a5d8bc5563f75605c77c60671c6f6488dae
Eyesonjune18 <lowellashton@gmail.com>
2022-12-16 22:08:01 -0800

    Added find_name() and contains().

--- src/utilities/symbol_table.rs
+++ src/utilities/symbol_table.rs
@@ -29,6 +29,22 @@ impl SymbolTable {
 
         None
     }
+
+    // Gets the corresponding label name in the symbol table for a given label address
+    pub fn find_name(&self, requested_address: u16) -> Option<String> {
+        for label in &self.labels {
+            if label.address == requested_address {
+                return Some(label.name);
+            }
+        }
+
+        None
+    }
+
+    // Checks if the symbol table already contains a label with the given address
+    pub fn contains(&self, address: u16) -> bool {
+        self.find_name(address).is_some()
+    }
 }
 
 // Represents a label, which is a name given to an address in instruction memory
